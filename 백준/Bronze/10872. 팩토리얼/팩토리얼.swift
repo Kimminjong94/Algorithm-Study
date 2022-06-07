cd4a1ab20ac7604f0a22e98b238fb48cc15aca63
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2022-06-07 21:01:37 +0900

    [Bronze V] Title: 팩토리얼, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.swift
@@ -0,0 +1,10 @@
+let num = Int(readLine()!)!
+
+func calculate (num : Int) -> Int {
+    if num == 0 {
+        return 1
+    }
+    return num * calculate(num: num - 1)
+}
+
+print(calculate(num: num))
