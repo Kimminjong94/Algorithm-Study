944c3b23ea6a58cec76822b33b7064675adb240b
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2022-06-12 00:02:09 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
@@ -0,0 +1,12 @@
+let T = Int(readLine()!)!
+var dp = Array(repeating: 0, count: 11)
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for _ in 1...T {
+    let n = Int(readLine()!)!
+    for i in stride(from: 4, through: n, by: 1) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    }
+    print("\(dp[n])")
+}
