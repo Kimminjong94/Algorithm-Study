cab9192c02822afb7756fac58a5bbdc9f4d9d6fb
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2022-06-11 23:54:15 +0900

    [Silver III] Title: 피보나치 함수, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.swift
@@ -0,0 +1,22 @@
+func solution(){
+    let t = Int(String(readLine()!))!
+    var test: [Int] = []
+    
+    for _ in 0..<t {
+        test.append(Int(String(readLine()!))!)
+    }
+    
+    var result: [(Int,Int)] = [(Int,Int)](repeating: (0,0), count: 41)
+    result[0] = (1,0)
+    result[1] = (0,1)
+    
+    for i in 2..<result.count{
+        result[i] = (result[i-1].0 + result[i-2].0, result[i-1].1 + result[i-2].1)
+    }
+    
+    for num in test {
+        print("\(result[num].0) \(result[num].1)")
+    }
+}
+
+solution()
