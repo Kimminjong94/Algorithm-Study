7e79259e92f72648ad50f9d5c2c2a882b13ab652
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2022-06-17 22:49:15 +0900

    [Silver I] Title: RGB거리, Time: 12 ms, Memory: 69236 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.swift
@@ -0,0 +1,15 @@
+let n = Int(String(readLine()!))!
+var arr: [[Int]] = [[]]
+var dp: [[Int]] = Array(repeating: Array(repeating: 0, count: 4), count: n + 1)
+for _ in 1...n {
+    let a = readLine()!.split(separator: " ").map{Int(String($0))!}
+    arr.append(a)
+}
+
+for i in 1...n {
+    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0]
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1]
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2]
+}
+
+print(min(dp[n][0], dp[n][1], dp[n][2]))
