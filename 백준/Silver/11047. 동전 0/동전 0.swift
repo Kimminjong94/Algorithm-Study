c31b74693191bbf7c6f00d43664545f757139cfd
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2022-06-24 23:35:06 +0900

    [Silver IV] Title: 동전 0, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.swift
@@ -0,0 +1,29 @@
+func solution() -> Int {
+    
+    let temp = readLine()!.split(separator: " ").map({Int($0)!})
+    let n = temp[0]
+    var k = temp[1]
+    
+    var coins = [Int]()
+    var result: Int = 0
+    
+    for _ in 0..<n{
+        coins.append(Int(readLine()!)!)
+    }
+    
+    coins = Array(coins.reversed())
+    
+    for value in coins{
+        if value <= k{
+            result += (k / value)
+            k = (k % value)
+        }
+        if k == 0{
+            break
+        }
+    }
+    
+    return result
+}
+
+print(solution())
