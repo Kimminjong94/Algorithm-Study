edda2613e50dcd21819595c2e58822bbc3838393
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2022-07-06 22:49:22 +0900

    [Silver II] Title: A → B, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.swift
@@ -0,0 +1,23 @@
+let t = readLine()!.split(separator: " ").map{Int(String($0))!}
+var (A,B) = (t[0],t[1])
+
+var cnt = 1
+
+while A < B {
+    if B % 2 == 0 {
+        B /= 2
+    }
+    else if B%10 == 1 {
+        B /= 10
+    }
+    else {
+        break
+    }
+    cnt += 1
+}
+
+if A == B {
+    print(cnt)
+} else {
+    print(-1)
+}
