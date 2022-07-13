2ff0401b9d8028ab26ea2f7d0f3d9e71fc113c52
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2022-07-13 17:42:25 +0900

    [Bronze V] Title: A+B - 3, Time: 12 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.swift
@@ -0,0 +1,12 @@
+var line = Int(readLine()!)!
+
+
+for _ in 1...line {
+    let input = readLine()!.split(separator: " ")
+
+    let inputA = Int(input[0])!
+    let inputB = Int(input[1])!
+    
+     print(inputA + inputB)
+
+}
