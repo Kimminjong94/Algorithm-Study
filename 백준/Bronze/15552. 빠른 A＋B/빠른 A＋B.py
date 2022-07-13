3adc6319b8cad4b0c3d82fdd611a5f8e5e5af9dd
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2022-07-13 19:34:17 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1372 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -1,6 +1,7 @@
-import sys
- 
-inp = int(input())
-for i in range(inp):
-        a,b = map(int, sys.stdin.readline().split())
-        print(a+b)
+import sys  # sys모듈 읽어들이기
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    a,b = map(int, sys.stdin.readline().split())
+    print(a+b)
