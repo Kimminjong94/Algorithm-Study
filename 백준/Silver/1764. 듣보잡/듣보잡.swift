88060d705f431c92673fc375b20a66c4a770e2b3
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2022-09-02 14:13:09 +0900

    [Silver IV] Title: 듣보잡, Time: 104 ms, Memory: 81952 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.swift
@@ -0,0 +1,12 @@
+let input = readLine()!.split(separator: " ").map{Int($0)!}
+var set1 = Set<String>()
+var set2 = Set<String>()
+(0..<input[0]).forEach{ _ in
+    set1.insert(readLine()!)
+}
+(0..<input[1]).forEach { _ in
+    set2.insert(readLine()!)
+}
+let arr = set1.intersection(set2).sorted()
+print(arr.count)
+print(arr.joined(separator: "\n"))
