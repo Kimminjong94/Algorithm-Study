d959c076bf9d9762c9cf23f8d1aa0f2593b89057
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2022-11-29 23:46:29 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 16 ms, Memory: 69636 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.swift
@@ -0,0 +1,30 @@
+let input = readLine()!.split { $0 == " " }
+var inputA = Array(input[0]).map { Int(String($0))! }
+var inputB = Array(input[1]).map { Int(String($0))! }
+
+if inputA.count > inputB.count {
+    let diff = inputA.count - inputB.count
+    for _ in 0 ..< diff {
+        inputB.insert(0, at: inputB.startIndex)
+    }
+} else {
+    let diff = inputB.count - inputA.count
+    for _ in 0 ..< diff {
+        inputA.insert(0, at: inputA.startIndex)
+    }
+}
+
+for i in 0 ..< inputA.count {
+    inputA[i] += inputB[i]
+}
+
+for i in (1 ..< inputA.count).reversed() {
+    if inputA[i] >= 10 {
+        inputA[i-1] += 1
+    }
+    inputA[i] = inputA[i] % 10
+}
+
+var stringA = inputA.map { String($0) }
+let result = stringA.joined(separator: "")
+print(result)
