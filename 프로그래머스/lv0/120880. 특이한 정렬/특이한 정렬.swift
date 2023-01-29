63b8e3e87857194e8bc09f3adefc8c4021ebcbb6
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2023-01-29 14:50:51 +0900

    [level 0] Title: 특이한 정렬, Time: 0.08 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ numlist:[Int], _ n:Int) -> [Int] {
+    
+    var newArray:[Int] = numlist.map{$0 - n}.sorted {
+        
+        if abs($0) == abs($1) {
+            return $0 > $1
+        } else {
+            return abs($0) < abs($1)
+        }
+    }
+    
+    return newArray.map{$0 + n}
+}
