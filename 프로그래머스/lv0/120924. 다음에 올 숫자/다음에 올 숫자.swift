c9d338948b657487637b32fba6134ab8413c32a2
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2023-02-04 15:37:18 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.swift
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.swift
@@ -1,11 +1,15 @@
 import Foundation
 
+// func solution(_ common:[Int]) -> Int {
+//     //등차수열일때
+//     if common[1] - common[0] == common[2] - common[1] {
+//         return common.last! + common[1] - common[0]
+//     //등비수열일때
+//     } else {
+//         return common.last! * common[1] / common[0]
+//     }
+// }
+
 func solution(_ common:[Int]) -> Int {
-    //등차수열일때
-    if common[1] - common[0] == common[2] - common[1] {
-        return common.last! + common[1] - common[0]
-    //등비수열일때
-    } else {
-        return common.last! * common[1] / common[0]
-    }
+    common[1] - common[0] == common[2] - common[1] ? common.last! + common[1] - common[0] : common.last! * common[1] / common[0]
 }
