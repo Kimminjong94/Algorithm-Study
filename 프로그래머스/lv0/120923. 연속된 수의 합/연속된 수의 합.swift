0c2b18346b80e6361edf60279e1f81cf8349a10f
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2023-02-07 22:03:24 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.10 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.swift
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.swift
@@ -3,14 +3,12 @@ import Foundation
 func solution(_ num:Int, _ total:Int) -> [Int] {
     
  
-    var exis = 0
     var newNum = total
-    
+
     for i in 0..<num {
-        exis += 1
         newNum -= i
         	
     }
     
-    return Array((newNum / exis)...(num + (newNum / exis) - 1))
+    return Array((newNum / num)...(num + (newNum / num) - 1))
 }
