5588c7078d8c0e6db07e73eb2eb434957260c538
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2023-03-14 22:22:44 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.swift
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.swift
@@ -1,12 +1,3 @@
 func solution(_ arr:[Int]) -> Double {
-    
-    // let sum: Int = arr.reduce(0) {$0 + $1}
-    // var countNum = arr.count
-    // var average = Double(sum) / Double(countNum)
-    
-    let sum = Double(arr.reduce(0, +)) / Double(arr.count)
-    
-    
-    
-    return sum
+    Double(arr.reduce(0, +)) / Double(arr.count)
 }
