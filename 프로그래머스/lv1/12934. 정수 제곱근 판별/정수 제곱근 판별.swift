e5a9b3fb272f8ea7c8b8c38a6c0b5cfb0ea44b80
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2023-04-04 09:45:26 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.swift
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.swift
@@ -1,10 +1,13 @@
 func solution(_ n:Int64) -> Int64 {
    
+
     for i in 1...n {
         if i * i == n {
             return (i + 1) * (i + 1)
         }
     }
+    
+    
     
     return -1
 }
