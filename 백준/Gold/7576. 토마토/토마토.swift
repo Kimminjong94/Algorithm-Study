20e5a6476842f0f89c10491704383e8b68cb65b1
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2025-02-10 15:27:47 +0900

    [Gold V] Title: 토마토, Time: 304 ms, Memory: 110296 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.swift
+++ 백준/Gold/7576. 토마토/토마토.swift
@@ -1,49 +1,67 @@
-import Foundation
+let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
 
-let size = readLine()!.split(separator: " ").map({Int(String($0))!}) // 상자 크기
-let m = size[0] // 가로 칸 수
-let n = size[1] // 세로 칸 수
-var box = [[Int]]() // 주어진 박스
-var queue = [(Int, Int)]()
-let dx = [-1, 1, 0 , 0], dy = [0, 0, -1, 1] // 상하좌우 검색을 위한 좌표 변화량
-var empty = 0 // 0으로 채워진 자리들의 개수를 누적!
-var count = 0 // 0을 1로 바꾸는 횟수를 누적!
+//정점의 갯수로 배열을 만듬
+var graph:[[Int]] = Array(repeating: [], count: nm[1])
+var visited:[[Bool]] = Array(repeating: Array(repeating: false, count: nm[0]), count: nm[1])
 
-for _ in 0..<n {
-    box.append(readLine()!.split(separator: " ").map({Int(String($0))!}))
+for i in 0..<nm[1] {
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    graph[i].append(contentsOf: input)
 }
 
-// bfs 함수 구현
-func bfs() {
-    var index = 0 // queue를 removeFirst하니까 시간초과,, => 굳이 제거하지않고 인덱스로 찾아줌.
+
+// 이동 방향 (상, 하, 좌, 우)
+let dx = [0, 0, -1, 1]
+let dy = [-1, 1, 0, 0]
+
+func bfs(_ xy: [(Int, Int)]) -> Int {
+    var queue = xy
+    var index = 0
+    var daysPassed = 1
+
     
     while index < queue.count {
-        let (x, y) = queue[index]
+        let nowNode = queue[index]
         index += 1
+        visited[nowNode.0][nowNode.1] = true
         
         for i in 0..<4 {
-            let nx = x + dx[i]
-            let ny = y + dy[i]
+            let nextX = nowNode.0 + dx[i]
+            let nextY = nowNode.1 + dy[i]
             
-            if nx < 0 || ny < 0 || nx >= n || ny >= m { continue } // 예외
-            
-            if box[nx][ny] == 0 {
-                queue.append((nx, ny)) // 1로 바뀌었으므로 큐에 추가
-                box[nx][ny] = box[x][y] + 1 // 1로 바꾸면서 걸린 시간 누적
-                count += 1 // 0에서 1로 바꾼 횟수 누적
+            if nextX >= 0, nextY >= 0, nextX < nm[1], nextY < nm[0], graph[nextX][nextY] == 0, !visited[nextX][nextY] {
+                graph[nextX][nextY] = graph[nowNode.0][nowNode.1] + 1
+                daysPassed = graph[nowNode.0][nowNode.1] + 1
+                visited[nextX][nextY] = true
+                queue.append((nextX, nextY))
             }
         }
     }
-}
 
-// 메인 코드 실행 전, 박스에 있는 익은 토마토 및 안익은 토마토 체크
-for x in 0..<n {
-    for y in 0..<m {
-        if box[x][y] == 1 { queue.append((x, y)) } // 1이면 큐에 추가
-        else if box[x][y] == 0 { empty += 1 } // 0이면 empty에 개수 누적
+    for row in graph {
+        if row.contains(0) {
+            return -1
+        }
     }
+
+    
+    return daysPassed - 1
 }
 
-bfs() // 함수 실행
+func solution() {
+    
+    var coor = [(Int, Int)]()
 
-empty == 0 ? print(0) : (empty == count ? print(box.flatMap({$0}).max()!-1) : print(-1))
+    for i in 0..<nm[1] {
+        for j in 0..<nm[0] {
+            if graph[i][j] == 1 {
+                coor.append((i, j))
+            }
+        }
+    }
+    
+    let answer = bfs(coor)
+    print(answer)
+}
+
+solution()
