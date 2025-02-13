e6d4780e535c88e4203f77fe7f61852f09d0e3bf
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2025-02-13 14:06:19 +0900

    [Silver III] Title: 피보나치 함수, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.swift
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.swift
@@ -1,22 +1,21 @@
-func solution(){
-    let t = Int(String(readLine()!))!
-    var test: [Int] = []
-    
-    for _ in 0..<t {
-        test.append(Int(String(readLine()!))!)
-    }
-    
-    var result: [(Int,Int)] = [(Int,Int)](repeating: (0,0), count: 41)
-    result[0] = (1,0)
-    result[1] = (0,1)
-    
-    for i in 2..<result.count{
-        result[i] = (result[i-1].0 + result[i-2].0, result[i-1].1 + result[i-2].1)
-    }
-    
-    for num in test {
-        print("\(result[num].0) \(result[num].1)")
-    }
+let N = Int(readLine()!)!
+var dp = Array(repeating: (0,0), count: 41)
+
+for _ in 0..<N {
+    let input = Int(readLine()!)!
+    print(solution(input).0, solution(input).1)
 }
 
-solution()
+
+
+
+func solution(_ n: Int) -> (Int, Int) {
+    
+    dp[0] = (1, 0)
+    dp[1] = (0, 1)
+    
+    for i in 2..<41 {
+        dp[i] = ((dp[i - 1].0 + dp[i - 2].0), (dp[i - 1].1 + dp[i - 2].1))
+    }
+    return dp[n]
+}
