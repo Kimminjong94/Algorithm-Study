c2a5900016887aed2d83a365e00c8aa63ad01ebc
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2025-02-13 13:42:05 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
@@ -1,12 +1,21 @@
-let T = Int(readLine()!)!
-var dp = Array(repeating: 0, count: 11)
-dp[1] = 1
-dp[2] = 2
-dp[3] = 4
-for _ in 1...T {
-    let n = Int(readLine()!)!
-    for i in stride(from: 4, through: n, by: 1) {
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+let N = Int(readLine()!)!
+var dp = Array(repeating: 0, count: 13)
+
+for _ in 0..<N {
+    let input = Int(readLine()!)!
+    print(solution(input))
+    
+}
+
+func solution(_ n: Int) -> Int {
+    
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+    
+    for i in 4..<13 {
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
     }
-    print("\(dp[n])")
+        
+    return dp[n]
 }
