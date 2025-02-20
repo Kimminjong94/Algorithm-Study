93d34bf84ad899fcced278bf327c91e0f6ba694a
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2025-02-20 10:48:11 +0900

    [Bronze II] Title: 수 정렬하기, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.swift
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.swift
@@ -1,21 +1,11 @@
 let n = Int(readLine()!)!
-var arr: [Int] = []
+var numbers:[Int] = []
 
 for _ in 0..<n {
-    arr.append(Int(readLine()!)!)
-    
+    let input = Int(readLine()!)!
+    numbers.append(input)
 }
 
-for i in 0..<n {
-    for j in i + 1..<n {
-        if arr[i] > arr[j] {
-            let temp = arr[i]
-            arr[i] = arr[j]
-            arr[j] = temp
-        }
-    }
-}
-
-for k in 0..<n {
-    print("\(arr[k])")
+for i in numbers.sorted() {
+    print(i)
 }
