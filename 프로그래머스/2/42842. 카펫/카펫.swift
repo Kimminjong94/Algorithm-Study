ddb50c123c4bbe54ea7962216aa81b8ea3659184
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2025-03-11 11:39:36 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.swift
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -1,16 +1,20 @@
 import Foundation
 
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
-    var result = [0, 0]
-    let S = brown + yellow
-    for width in stride(from: 1, through: S - 1, by: 1){
-        if S % width != 0 {continue}
-        let height = S / width
-        if width < height{continue}
-        if (width - 2) * (height - 2) == yellow{
-            result = [width, height]
+    
+    for height in 1...yellow {
+        if yellow % height != 0 {
+            continue
+        }
+    
+        let width = yellow / height
+                
+        let topBottomSize = (width + 2) * 2
+        let sideSize = height * 2
+        
+        if topBottomSize + sideSize == brown {
+            return [topBottomSize / 2, (sideSize / 2) + 2]
         }
     }
-    
-    return result
+    return []
 }
