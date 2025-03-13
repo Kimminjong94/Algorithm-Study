8609f96d72ccf4f38dd71b86cc7dfd78771fa5fd
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2025-03-13 11:21:43 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.60 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.swift
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.swift
@@ -1,21 +1,19 @@
 import Foundation
 
 func solution(_ n:Int, _ words:[String]) -> [Int] {
+    var saidWord:[String] = [words[0]]
+
     
-    var checkDuplicateWords = [words[0]]
-    
-    for i in 1...words.count - 1 {
-        // 1. 중복 단어 체크
-        if checkDuplicateWords.contains(words[i]) {
-            return [(i % n) + 1, (i / n) + 1]
-        }
-        checkDuplicateWords.append(words[i])
-        
-        // 2. 끝말잇기가 제대로 진행 되는 지 체크 후 틀린 사람 체크
-        if words[i].first != words[i-1].last {
-            return [(i % n) + 1, (i / n) + 1]
+    for i in 1..<words.count {
+        guard !saidWord.contains(words[i]) else {
+            return[(i + 1) % n == 0 ? n : (i + 1) % n, (i + 1) % n == 0 ? (i + 1) / n : ((i + 1) / n) + 1]
         }
         
+        if saidWord.last!.last! == words[i].first! {
+            saidWord.append(words[i])
+        } else {
+            return [(i + 1) % n == 0 ? n : (i + 1) % n,(i + 1) % n == 0 ? (i + 1) / n : ((i + 1) / n ) + 1]
+        }
     }
     return [0, 0]
 }
