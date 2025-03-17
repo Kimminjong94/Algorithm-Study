3630b0cc99c96143730acc66dd5b7692fd2abbd5
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2025-03-17 10:17:46 +0900

    [Silver III] Title: 01타일, Time: 60 ms, Memory: 76916 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.swift
@@ -0,0 +1,21 @@
+let n = Int(readLine()!)!
+var dp = Array(repeating: 0, count: n + 1)
+
+func sol() -> Int {
+    if n == 1 {
+        return 1
+    } else if n == 2 {
+        return 2
+    }
+    
+    dp[1] = 1
+    dp[2] = 2
+    
+    for i in 3..<n + 1 {
+        dp[i] = (dp[i - 2] + dp[i - 1]) % 15746
+    }
+        
+    return dp[n]
+}
+
+print(sol())
