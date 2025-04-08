b89afd06b67de2c6e96d24065b761d5a5ef80efc
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2025-04-08 10:33:35 +0900

    [Silver III] Title: 2×n 타일링, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.swift
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.swift
@@ -1,24 +1,22 @@
-let N = Int(readLine()!)!
-var dp = Array(repeating: 0, count: 1001)
+let n = Int(readLine()!)!
+var dp = Array(repeating: 0, count: n + 1)
 
-func solution(_ n: Int) -> Int {
-    if n < 2 {
+func sol() -> Int {
+    if n == 1 {
         return 1
-    }
-    
-    if n < 3 {
+    } else if n == 2 {
         return 2
     }
     
     dp[1] = 1
     dp[2] = 2
     
-    for i in 3...N {
-        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
-    }
     
+    for i in 3..<n + 1 {
+        dp[i] = (dp[i - 2] + dp[i - 1]) % 10007
+    }
     return dp[n]
-   
+    
 }
 
-print(solution(N))
+print(sol())
