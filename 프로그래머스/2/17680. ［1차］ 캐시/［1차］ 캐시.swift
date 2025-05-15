f6ad94f3d84c33f070eb9787207177ebc5d55fb3
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2025-05-15 16:19:49 +0900

    [level 2] Title: [1차] 캐시, Time: 556.81 ms, Memory: 21.8 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
@@ -1,25 +1,27 @@
 import Foundation
 
-func solution(_ cacheSize: Int, _ cities: [String]) -> Int{
-    let tmp = cities.map{ $0.uppercased() }
-    var count = 0
-    var cache = [String]()
+func solution(_ cacheSize: Int, _ cities: [String]) -> Int {
     
-    for city in tmp{
-        if cache.contains(city){
-            count += 1
-            
-            cache.remove(at: cache.firstIndex(of: city)!)
-            cache.append(city)
-        } else {
+    guard cacheSize != 0 else {return 5 * cities.count}
+
+    var cities = cities.map{$0.lowercased()}
+    var tmpCities: [String] = []
+    var count = 0
+    
+    for city in cities {
+        
+        if tmpCities.count < cacheSize && !tmpCities.contains(city) {
+            tmpCities.append(city)
             count += 5
-            cache.append(city)
-            if cache.count > cacheSize{
-                cache.removeFirst()
-            }
+        } else if tmpCities.contains(city) {
+            tmpCities.removeAll { $0 == city}
+            tmpCities.append(city)
+            count += 1
+        } else {
+            tmpCities.removeFirst()
+            tmpCities.append(city)
+            count += 5
         }
     }
-    
     return count
-    
 }
