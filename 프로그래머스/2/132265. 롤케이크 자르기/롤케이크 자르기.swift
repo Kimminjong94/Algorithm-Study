0fe51146fd6c28a4da8b32be9276398988f6d9c4
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2025-05-26 10:59:15 +0900

    [level 2] Title: 롤케이크 자르기, Time: 1058.47 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.swift
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.swift
@@ -2,30 +2,27 @@ import Foundation
 
 func solution(_ topping:[Int]) -> Int {
     var answer = 0
-    var chulsooDict = [Int: Int]()
-    var brotherDict = [Int: Int]()
+    var leftDict = [Int: Int]()
+    var rightDict = [Int: Int]()
+
+    // 처음엔 모든 토핑을 오른쪽에 넣어둠
+    for t in topping {
+        rightDict[t, default: 0] += 1
+    }
 
     for t in topping {
-          brotherDict[t, default: 0] += 1
-      }
-    
-    for t in topping {
-        // 철수에게 토핑 추가
-        chulsooDict[t, default: 0] += 1
-        
-        // 동생의 토핑 개수 감소
-        if let count = brotherDict[t] {
-            if count == 1 {
-                brotherDict[t] = nil  // 마지막 남은 토핑이면 삭제
-            } else {
-                brotherDict[t]! -= 1
-            }
+        // 왼쪽으로 옮기기
+        leftDict[t, default: 0] += 1
+        rightDict[t]! -= 1
+        if rightDict[t]! == 0 {
+            rightDict.removeValue(forKey: t)
         }
-        
-        if chulsooDict.count == brotherDict.count {
+
+        // 두 쪽의 서로 다른 토핑 수 비교
+        if leftDict.count == rightDict.count {
             answer += 1
         }
     }
-      
+
     return answer
 }
