c71ba73e940344e1219f7fa22cc139aeb226513d
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2025-06-20 10:51:48 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
@@ -1,17 +1,14 @@
 import Foundation
 
-func solution(_ n:Int, _ a:Int, _ b:Int) -> Int
-{
-    var A = a % 2 != 0 ? a + 1: a
-    var B = b % 2 != 0 ? b + 1: b
+func solution(_ n:Int, _ a:Int, _ b:Int) -> Int {
+    var a = a
+    var b = b
+    var count = 0
     
-    var answer = 1
-    
-    while A != B{
-        A = (A / 2) % 2 != 0 ? (A / 2) + 1 : A / 2
-        B = (B / 2) % 2 != 0 ? (B / 2) + 1 : B / 2
-        answer += 1
+    while a != b {
+        a = (a + 1) / 2
+        b = (b + 1) / 2
+        count += 1
     }
-
-    return answer
+    return count
 }
