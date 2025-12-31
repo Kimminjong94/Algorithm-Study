dd06fe1695d097c5e44ed83ef8005167ddbe0640
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2025-12-31 14:26:22 +0900

    [level 2] Title: 행렬의 곱셈, Time: 29.31 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.swift
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.swift
@@ -1,19 +1,16 @@
 import Foundation
-
-func solution(_ arr1: [[Int]], _ arr2: [[Int]]) -> [[Int]] {
-    let rowCount = arr1.count
-    let colCount = arr2[0].count
-    let innerCount = arr2.count
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    // 두 행렬의 곱셈 결과에 맞춰서 신규 배열을 구성하고 초기화한다.
+    // 예) [2x3] * [2x2] = [2x3] => 두 행렬 중 큰 크기의 갯수로 정답 행렬 구성 
+    var answer = Array(repeating: Array(repeating: 0, count: arr2[0].count), count: arr1.count)
     
-    var result = Array(repeating: Array(repeating: 0, count: colCount), count: rowCount)
-    
-    for i in 0..<rowCount {
-        for j in 0..<colCount {
-            for k in 0..<innerCount {
-                result[i][j] += arr1[i][k] * arr2[k][j]
+    for i in 0..<answer.count {
+        for j in 0..<answer[i].count {
+            for k in 0..<arr1[0].count {
+                answer[i][j] += arr1[i][k] * arr2[k][j]
             }
         }
     }
-    
-    return result
+
+    return answer
 }
