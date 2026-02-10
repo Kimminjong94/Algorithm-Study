6fe326762533e7515306b5ec2eda2b59687f8432
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2026-02-10 10:36:26 +0900

    [Silver I] Title: IOIOI, Time: 48 ms, Memory: 85716 KB, Score: 100 point -BaekjoonHub

--- Swift/백준/Silver/5525. IOIOI/IOIOI.swift
+++ Swift/백준/Silver/5525. IOIOI/IOIOI.swift
@@ -1,21 +1,22 @@
-import Foundation
-
 let N = Int(readLine()!)!
-let M = Int(readLine()!)!
+let _ = Int(readLine()!)!
 let S = Array(readLine()!)
 
-var Pn:String = "I"
-for _ in 0..<N {
-    Pn.append("OI")
-}
-
-let checkCount = Pn.count
+var i = 0
 var count = 0
+var answer = 0
 
-for i in 0...M - checkCount {
-    let tmpArr = String(S[i..<i+checkCount])
-    if tmpArr == Pn {
+while i < S.count - 2 {
+    if S[i] == "I" && S[i+1] == "O" && S[i+2] == "I" {
         count += 1
+        if count >= N {
+            answer += 1
+        }
+        i += 2
+    } else {
+        count = 0
+        i += 1
     }
 }
-print(count)
+
+print(answer)
