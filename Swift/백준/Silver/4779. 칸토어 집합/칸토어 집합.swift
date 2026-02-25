e8830f92e067ff56c353f67e0f7f6fbc0940d2a6
MinjongKim <79985912+Kimminjong94@users.noreply.github.com>
2026-02-25 10:59:41 +0900

    [Silver III] Title: 칸토어 집합, Time: 24 ms, Memory: 81152 KB -BaekjoonHub

--- Swift/백준/Silver/4779. 칸토어 집합/칸토어 집합.swift
+++ Swift/백준/Silver/4779. 칸토어 집합/칸토어 집합.swift
@@ -1,12 +1,22 @@
+
 import Foundation
 
-func cantor(_ n: Int) -> String {
-    if n == 0 { return "-" }
-    let prev = cantor(n - 1)
-    return prev + String(repeating: " ", count: prev.count) + prev
+var dp = Array(repeating: "", count: 13)
+dp[0] = "-"
+
+
+for i in 1..<13 {
+    let emptyCount = String(repeating: " ", count: dp[i-1].count)
+    dp[i] = dp[i-1] + emptyCount + dp[i-1]
 }
 
-// EOF까지 입력 받기
-while let line = readLine(), let n = Int(line) {
-    print(cantor(n))
+
+while true {
+   let N = readLine() ?? "finished"
+    
+    if N == "finished" {
+        break
+    }
+     
+    print(dp[Int(N)!])
 }
